a93f99163ee03730a99dab1aac6224ccfb4374f6
Kingriver <945815601@qq.com>
2018-12-05 17:31:56 +0800

    vue系统

---
+++ cifs_system/cifs_admin/src/pages/text.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text">
+      内容........
+  </div>
+</template>
+
+<script>
+export default {
+   name:'Text', 
+   components:{},
+   props:{},
+   data(){
+     return {
+     }
+   },
+   watch:{},
+   computed:{},
+   methods:{},
+   created(){},
+   mounted(){}
+}
+</script>
+<style lang="less" scoped>
+</style>
