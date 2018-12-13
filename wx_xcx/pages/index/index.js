6ecce348de60042b9ea6946a020fa8144a577f23
Kingriver <945815601@qq.com>
2018-12-13 18:07:43 +0800

    s

--- wx_xcx/pages/index/index.js
+++ wx_xcx/pages/index/index.js
@@ -1,58 +1,70 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var appId ='wx36f66757564195a9';
+var secret ='2a3fb080ae3e486a5cbab1fd7b646b9b'
 Page({
   data: {
-    imgs:[
-      { src:'http://images.winipo.cn/Public/banner/2017/10/17/20171017102524-f4lRkvIT.jpg'},
-      { src: 'http://images.winipo.cn/Public/banner/2018/01/12/20180112173859-8s5Iy7El.jpg' }
-    ],
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    token:''
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  onLoad: function () {
+    this.getToken()
+  },
+  getToken(){
+    var that=this;
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx36f66757564195a9&secret=2a3fb080ae3e486a5cbab1fd7b646b9b',
+      success:function(res){
+        var token = res.data.access_token;
+        console.log(res)
+        that.getQrCode(token)
+        that.setData({
+          token: token
+        })
+      },
+      fail:function(err){
+        console.log(err)
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  getQrCode(token){
+    var that = this;
+    var pagePaths = getCurrentPages();
+    console.log(pagePaths[pagePaths.length - 1])
+    var currentPath = (pagePaths[pagePaths.length - 1]).route;
+    console.log(currentPath)
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token='+token,
+      data:{
+        path: currentPath
+      },
+      method:'POST',
+      success: function (res) {
+        wx.request({
+          url: 'http://www.test.com/table.php',
+          method:'POST',
+           header: {
+             'content-type': 'application/x-www-form-urlencoded' // 默认值
+           },
+          data:{
+            token: token,
+            url: currentPath
+          },
+          success:function(res){
+            console.log(res)
+          },
+          fail:function(err){
+            console.log(err)
+          }
         })
+       
+        
+      },
+      fail: function (err) {
+        console.log(err)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
+
 })
