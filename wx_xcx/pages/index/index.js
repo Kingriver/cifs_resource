d258748e7cb92133e22bdf9a84201f89a17dbf50
Kingriver <945815601@qq.com>
2018-12-17 17:38:01 +0800

    xcx

--- wx_xcx/pages/index/index.js
+++ wx_xcx/pages/index/index.js
@@ -1,30 +1,44 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var appId ='wx36f66757564195a9';
-var secret ='2a3fb080ae3e486a5cbab1fd7b646b9b'
+// var appId ='wx36f66757564195a9';
+// var secret ='2a3fb080ae3e486a5cbab1fd7b646b9b';
 Page({
   data: {
   },
 
   onLoad: function () {
-    this.getQrCode();
+   // this.getQrCode();
+    this.getToken()
+  },
+
+  getToken(token) {
+    var that=this;
+    wx.request({
+      url: 'http://www.wx.com/xcx/user_token.php',
+      success: function (res) {
+        console.log(res);
+        that.getQrCode()
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
   },
 
   getQrCode(token){
     var that = this;
     var pagePaths = getCurrentPages();
-    console.log(pagePaths[pagePaths.length - 1])
     var currentPath = (pagePaths[pagePaths.length - 1]).route;
-    console.log(currentPath)
     wx.request({
       url: 'http://www.wx.com/xcx/qr_code.php',
       method: 'POST',
-      // header: {
-      //   'content-type': 'application/x-www-form-urlencoded' // 默认值
-      // },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值 application/json
+      },
       data: {
-        url: currentPath
+        path: currentPath,
+        style:'check'
       },
       success: function (res) {
         console.log(res)
